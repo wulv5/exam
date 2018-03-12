d535abba77be9406748ab12b8fb04d5be5761f5f
wulv5 <wulv5@outlook.com>
2018-03-12 13:37:31 +0800

    Update service-worker.js

--- www/service-worker.js
+++ www/service-worker.js
@@ -5,21 +5,21 @@
 
 
 'use strict';
-importScripts('./build/sw-toolbox.js');
+// importScripts('./build/sw-toolbox.js');
 
-self.toolbox.options.cache = {
-  name: 'ionic-cache'
-};
+// self.toolbox.options.cache = {
+//   name: 'automatic-import'
+// };
 
 // dynamically cache any other local assets
-self.toolbox.router.any('/!*', self.toolbox.fastest);
+// self.toolbox.router.any('/!*', self.toolbox.fastest);
 
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
-self.toolbox.router.default = self.toolbox.networkFirst;
+// self.toolbox.router.default = self.toolbox.networkFirst;
 
 
-const cacheName = 'tz-web-exam-v0.0.1';
+const cacheName = 'tz-web-exam-v0.0.2';
 const filesToCache = [
   './',
   './index.html',
@@ -27,7 +27,7 @@ const filesToCache = [
   './build/vendor.js',
   './build/main.css',
   './build/polyfills.js',
-  './build/sw-toolbox.js',
+  // './build/sw-toolbox.js',
   './manifest.json',
   './assets/imgs/icon.png',
   './assets/imgs/home/html.png',
@@ -44,22 +44,18 @@ const filesToCache = [
   './assets/icon/login.svg',
   './assets/icon/password.svg',
 ];
-// 用户首次访问页面时将会触发安装事件
+
 // 缓存资源
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] 安装');
   e.waitUntil(
-    // 打开缓存并提供一个缓存名称
-    // 提供缓存名称可让我们对文件进行版本控制，
-    // 或将数据与 App Shell 分开，以便我们能轻松地更新某个数据，而不会影响其他数据。
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
-      // 从服务器获取文件并添加到缓存内
       return cache.addAll(filesToCache);
     })
   );
 });
-// 事件会在服务工作线程启动时触发。
+
 // 检查更新
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
@@ -67,7 +63,7 @@ self.addEventListener('activate', function(e) {
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
+          console.log('[ServiceWorker] 更新', key);
           return caches.delete(key);
         }
       }));
@@ -75,7 +71,7 @@ self.addEventListener('activate', function(e) {
   );
   return self.clients.claim();
 });
-// 拦截网络请求 , 从缓存内读取资源
+// 从缓存中加载
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
